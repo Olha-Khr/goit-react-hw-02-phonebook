7396e1c4502508e96addc4ac7d5c4a851c7ad723
Olha <99271539+Olha-Khr@users.noreply.github.com>
2022-12-10 18:03:02 +0300

    styled

--- src/components/Contacts/Contacts.styled.js
+++ src/components/Contacts/Contacts.styled.js
@@ -6,14 +6,19 @@ export const FindContact = FormStyles.withComponent('div');
 
 export const DeleteBtn = styled.button`
   border: 0;
-  padding: 7px;
-  border-radius: 10px;
+  padding: 5px;
+  border-radius: 8px;
   margin-left: 10px;
   cursor: pointer;
+  color: red;
+  :focus{
+    border 2px solid red;
+  };
+
 `;
 
 export const Contact = styled.li`
   :not(:last-child) {
     margin-bottom: 7px;
-  }
+  };
 `;
--- src/components/Form/Form.styled.js
+++ src/components/Form/Form.styled.js
@@ -9,6 +9,9 @@ export const FormStyles = styled(Form)`
   flex-direction: column;
   gap: 8px;
   margin-bottom: 16px;
+  background-color: rgb(50, 80, 220);
+  border-radius: 80px/20px; 
+  color: yellow;
 `;
 
 export const Label = styled.label`
@@ -18,10 +21,16 @@ width: 250px;
   flex-direction: column;
   gap: 8px;
   margin-bottom: 8px;
+ 
 `;
 
 export const SubmitBtn = styled.button`
 padding: 8px;
-border-radius: 8px;
+border-radius: 5px;
 cursor: pointer;
-`;
+ background-color: yellow;
+:focus{
+    border 2px solid #000000;
+  };
+border:none;
+  `
